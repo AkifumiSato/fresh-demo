5d2566b84fbdcb23c9b7d7ffbcb7206233d2487f
佐藤昭文 <akfm.sato@gmail.com>
2022-09-07 23:39:01 +0900

    add vscode config

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
 /** @jsx h */
 import { h } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 
-export function Header({ children, ...props }: h.JSX.HTMLAttributes<HTMLButtonElement>) {
+export function Header({ children, ...props }: h.JSX.HTMLAttributes<HTMLHeadElement>) {
   return (
     <header {...props}>
       <div class={tw`flex justify-between	`}>
